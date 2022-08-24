2f69c3ec2689e7d3204604756610099f13055722
davidobr <david.obrien.nord@gmail.com>
2022-08-24 14:08:16 -0700

    Put the program into a method

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,17 +1,20 @@
 class FizzBuzz:
     value = ""
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            value = "fizzbuzz"
-            print(value)
-        elif i % 3 == 0:
-            value = "fizz"
-            print(value)
-        elif i % 5 == 0:
-            value = "buzz"
-            print(value)
-        else:
-            print(i)
+
+    def __init__(self):
+        for i in range(1, 101):
+            if i % 3 == 0 and i % 5 == 0:
+                value = "fizzbuzz"
+                print(value)
+            elif i % 3 == 0:
+                value = "fizz"
+                print(value)
+            elif i % 5 == 0:
+                value = "buzz"
+                print(value)
+            else:
+                value = i
+                print(value)
 
 
 fb = FizzBuzz()
