c4b945601acec24e5bf4a54844d0c360e2625aee
davidobr <david.obrien.nord@gmail.com>
2022-08-24 13:49:05 -0700

    Created FizzBuzz as a class

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,18 @@
-value = ""
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        value = "fizzbuzz"
-        print(value)
-    elif i % 3 == 0:
-        value = "fizz"
-        print(value)
-    elif i % 5 == 0:
-        value = "buzz"
-        print(value)
-    else:
-        print(i)
+class FizzBuzz:
+    value = ""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            value = "fizzbuzz"
+            print(value)
+        elif i % 3 == 0:
+            value = "fizz"
+            print(value)
+        elif i % 5 == 0:
+            value = "buzz"
+            print(value)
+        else:
+            print(i)
+
+
+fb = FizzBuzz()
+print(fb)
